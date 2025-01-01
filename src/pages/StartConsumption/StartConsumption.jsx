07aad86ad5bb327c1ef88e9c6daa7e0ab6bd6e87
sooa@naver.com <leesooa0316@naver.com>
2025-01-01 14:55:35 +0900

    기본 틀 완성

--- src/pages/StartConsumption/StartConsumption.jsx
+++ src/pages/StartConsumption/StartConsumption.jsx
@@ -1,8 +1,32 @@
 // 나의 소비 시작하기 페이지 구현 예정
 import React from "react";
+import Header from "../../components/Header/Header";
+import PrimaryButton from "../../components/Button/PrimaryButton";
 
 const StartConsumption = () => {
-  return <div>StartConsumption</div>;
+  const pageName = "나의 소비 시작하기";
+  return (
+    <div>
+      <Header pageName={pageName} />
+      <div>
+        <p>나의 소비 시작</p>
+        <p>일주일동안 사용하실 금액을 입력해주세요 !</p>
+        <p>
+          {/*span 부분 데이터 받아오기 */}
+          지난주에는 <span>10만원</span>을 사용했어요 !
+        </p>
+      </div>
+      <div>
+        <p>나의 소비 기간</p>
+      </div>
+      <div>
+        <p>목표 금액</p>
+      </div>
+      <PrimaryButton type="button" size="xl" disabled="true">
+        완료
+      </PrimaryButton>
+    </div>
+  );
 };
 
 export default StartConsumption;
