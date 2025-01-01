a80d3794db948cb7825d936b08a2dd6bd41d7a78
sooa@naver.com <leesooa0316@naver.com>
2025-01-01 22:17:42 +0900

    나의 소비 시작하기 기본 화면 구현 완료

--- src/pages/StartConsumption/StartConsumption.jsx
+++ src/pages/StartConsumption/StartConsumption.jsx
@@ -1,30 +1,41 @@
 // 나의 소비 시작하기 페이지 구현 예정
 import React from "react";
 import Header from "../../components/Header/Header";
+import styles from "./StartConsumption.module.css";
 import PrimaryButton from "../../components/Button/PrimaryButton";
 
 const StartConsumption = () => {
   const pageName = "나의 소비 시작하기";
   return (
-    <div>
+    <div className="wrapper">
       <Header pageName={pageName} />
-      <div>
-        <p>나의 소비 시작</p>
-        <p>일주일동안 사용하실 금액을 입력해주세요 !</p>
-        <p>
-          {/*span 부분 데이터 받아오기 */}
-          지난주에는 <span>10만원</span>을 사용했어요 !
-        </p>
+      <div className={styles.contentWrapper}>
+        <div className={styles.descWrapper}>
+          <h3>나의 소비 시작</h3>
+          <p>일주일동안 사용하실 금액을 입력해주세요 !</p>
+          <p>
+            {/*span 부분 데이터 받아오기 */}
+            지난주에는 <span>10만원</span>을 사용했어요 !
+          </p>
+        </div>
+        <div className={styles.inputWrapper}>
+          <p>나의 소비 기간</p>
+          <button type="button" className={styles.dayButton}>
+            <span>24.11.24 (월) ~ 24.11.31 (일)</span>
+          </button>
+        </div>
+        <div className={styles.inputWrapper}>
+          <p>목표 금액</p>
+          <button type="button" className={styles.moneyButton}>
+            <span>주소비 한도 금액을 입력해주세요</span>
+          </button>
+        </div>
       </div>
-      <div>
-        <p>나의 소비 기간</p>
+      <div className={styles.buttonWrapper}>
+        <PrimaryButton type="button" size="xl" disabled={true}>
+          완료
+        </PrimaryButton>
       </div>
-      <div>
-        <p>목표 금액</p>
-      </div>
-      <PrimaryButton type="button" size="xl" disabled="true">
-        완료
-      </PrimaryButton>
     </div>
   );
 };
