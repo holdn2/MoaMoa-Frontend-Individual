7a7ee9fd4ce0e8aecd26c801901c5aee6185478e
sooa@naver.com <leesooa0316@naver.com>
2025-01-03 21:16:20 +0900

    카테고리 설명 모달창 구현 완료

--- src/pages/InputConsumption/InputConsumption.jsx
+++ src/pages/InputConsumption/InputConsumption.jsx
@@ -6,6 +6,7 @@ import question from "../../assets/Content/question.svg";
 import Category from "./Category";
 import MoneyInput from "../../components/moneyInput/MoneyInput";
 import PrimaryButton from "../../components/Button/PrimaryButton";
+import DescModal from "./DescModal";
 
 const InputConsumption = () => {
   const pageName = "나의 소비 입력하기";
@@ -28,15 +29,21 @@ const InputConsumption = () => {
   const [isInputState, setIsInputState] = useState(false);
   const [categoryClicked, setCategoryClicked] = useState(0);
   const [challengeClicked, setChallengeClicked] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <div className={styles.wrapper}>
       <Header pageName={pageName} />
       <div className={styles.contentWrapper}>
+        {/*카테고리 버튼 부분 */}
         <div className={styles.categoryWrapper}>
           <div className={styles.categoryDesc}>
             <span>카테고리</span>
-            <img src={question} />
+            <img
+              src={question}
+              alt="카테고리 설명을 볼 수 있는 아이콘"
+              onClick={() => setModalOpen(true)}
+            />
           </div>
           <div className={styles.category}>
             {categoryName.map((name, index) => (
@@ -50,6 +57,9 @@ const InputConsumption = () => {
           </div>
           <p>*중복 선택이 불가능합니다.</p>
         </div>
+        {modalOpen && (
+          <DescModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+        )}
         <div className={styles.categoryWrapper}>
           <span>챌린지 카테고리</span>
           <div className={styles.category}>
@@ -64,6 +74,7 @@ const InputConsumption = () => {
           </div>
           <p>*중복 선택이 불가능합니다.</p>
         </div>
+        {/*input 부분*/}
         <div className={styles.inputWrapper}>
           <p>금액</p>
           <MoneyInput
@@ -72,6 +83,7 @@ const InputConsumption = () => {
             setIsInputState={setIsInputState}
           />
         </div>
+        {/*목표 금액까지 남은 금액 표시*/}
         <div className={styles.inputWrapper}>
           <p>목표 금액까지 남은 금액</p>
           <div className={styles.priceWrapper}>
@@ -79,11 +91,13 @@ const InputConsumption = () => {
             <span className={styles.priceDesc}>원</span>
           </div>
         </div>
-        <div className={styles.buttonWrapper}>
-          <PrimaryButton type="button" size="xl" disabled={!isInputState}>
-            완료
-          </PrimaryButton>
-        </div>
+        {!modalOpen && (
+          <div className={styles.buttonWrapper}>
+            <PrimaryButton type="button" size="xl" disabled={!isInputState}>
+              완료
+            </PrimaryButton>
+          </div>
+        )}
       </div>
     </div>
   );
