3277623c334c562df77b4d185f5217c27b106337
sooa@naver.com <leesooa0316@naver.com>
2025-01-03 20:27:46 +0900

    카테고리 선택 버튼까지 구현 완료

--- src/pages/StartConsumption/StartConsumption.jsx
+++ src/pages/StartConsumption/StartConsumption.jsx
@@ -4,7 +4,7 @@ import Header from "../../components/Header/Header";
 import styles from "./StartConsumption.module.css";
 import PrimaryButton from "../../components/Button/PrimaryButton";
 import SelectPeriod from "./SelectPeriod";
-import MoneyInput from "../../components/MoneyInput/MoneyInput";
+import MoneyInput from "../../components/moneyInput/MoneyInput";
 
 const StartConsumption = () => {
   const pageName = "나의 소비 시작하기";
