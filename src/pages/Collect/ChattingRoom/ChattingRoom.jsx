b82a9353c32b8bc030d5050df9c5b73de3ffe081
holdn2 <yoocy01@gmail.com>
2025-01-27 23:52:38 +0900

    채팅방 내 챌린지 클릭 시 수락 가능. 수락 이후 클릭 시에는 해당 챌린지 정보로 이동(이 부분은 추가 예정). 챌린지 없을 시에는 생성하러 가기로 이동.

--- src/pages/Collect/ChattingRoom/ChattingRoom.jsx
+++ src/pages/Collect/ChattingRoom/ChattingRoom.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ChattingRoom.module.css";
 import { useNavigate, useParams } from "react-router-dom";
 import Header from "../../../components/Header/Header";
 import ExitRoomModal from "./SideMenu/ExitRoomModal";
 import ChattingArea from "./DoChatting/ChattingArea";
+import AcceptChallengeModal from "./AcceptChallengeModal";
 
 const ChattingRoom = () => {
   const navigate = useNavigate();
@@ -17,12 +18,18 @@ const ChattingRoom = () => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+  // 챌린지 관련 정보 상태
+  const [challengeData, setChallengeData] = useState(currentChallenge);
+
+  useEffect(() => {
+    console.log(challengeData);
+  }, [challengeData]);
+
   // 채팅방 나가기 관련 모달상태
   const [isExitModalOpen, setIsExitModalOpen] = useState(false);
   const [exitRoom, setExitRoom] = useState(false); // 채팅방 나가기 상태
-
-  // 채팅방 내부 정보
-  const [chattingData, setChattingData] = useState([]);
+  // 챌린지 수락 관련 모달상태
+  const [isChallengeModalOpen, setIsChallengeModalOpen] = useState(false);
 
   return (
     <>
@@ -35,28 +42,35 @@ const ChattingRoom = () => {
             className={styles.RoomMenu}
             onClick={toggleMenu}
           />
-          {currentChallenge.length > 0 ? (
+          {challengeData ? (
             <div
               className={styles.ChallengeInfoContainer}
-              onClick={() => console.log("챌린지 보러가기")}
-              style={{ background: "#fff" }}
+              onClick={() => {
+                if (challengeData.isAccept) {
+                  console.log(
+                    "챌린지 보러가기. 챌린지 부분 완료되면 이후 추가 예정"
+                  );
+                } else {
+                  setIsChallengeModalOpen(true);
+                }
+              }}
             >
               <div className={styles.ChallengeTextContainer}>
                 <span className={styles.ChallengeTitle}>
-                  {currentChallenge[0].title}
+                  {challengeData.title}
                 </span>
                 <div style={{ display: "flex", gap: "14px" }}>
                   <span className={styles.ChallengeText}>
-                    {currentChallenge[0].coin}코인
+                    {challengeData.coin}코인
                   </span>
                   <span className={styles.ChallengeText}>
-                    {currentChallenge[0].date}
+                    {challengeData.date}
                   </span>
                   <span
                     className={styles.ChallengeText}
                     style={{ color: "#848484" }}
                   >
-                    {currentChallenge[0].people}명
+                    {challengeData.people}명
                   </span>
                 </div>
               </div>
@@ -69,7 +83,9 @@ const ChattingRoom = () => {
           ) : (
             <div
               className={styles.ChallengeInfoContainer}
-              onClick={() => console.log("챌린지 만들기")}
+              onClick={() =>
+                navigate(`/chatroom/${params.chatroomId}/roomchallenge`)
+              }
             >
               <div className={styles.ChallengeTextContainer}>
                 <span className={styles.ChallengeTitle}>
@@ -87,7 +103,12 @@ const ChattingRoom = () => {
             </div>
           )}
         </div>
-
+        <AcceptChallengeModal
+          isModalOpen={isChallengeModalOpen}
+          setIsModalOpen={setIsChallengeModalOpen}
+          challengeData={challengeData}
+          setChallengeData={setChallengeData}
+        />
         <div className={styles.MainArea}>
           <div>
             <ChattingArea />
@@ -228,12 +249,11 @@ const chatData = [
   },
 ];
 
-const currentChallenge = [
-  {
-    id: "asdf",
-    title: "1주일에 5만원으로 살아남기",
-    coin: 500,
-    date: "11/10 (수)",
-    people: 4,
-  },
-];
+const currentChallenge = {
+  id: "asdf",
+  title: "1주일에 5만원으로 살아남기",
+  coin: 500,
+  date: "11/10 (수)",
+  people: 4,
+  isAccept: false,
+};
