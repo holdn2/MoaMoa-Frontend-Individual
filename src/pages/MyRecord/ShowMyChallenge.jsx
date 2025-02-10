2ff0b3f9bcb507e1831902c04cef33d2c1b89379
holdn2 <yoocy01@gmail.com>
2025-02-10 21:58:22 +0900

    fix : 나의 소비 기록 확인하기 페이지 스타일링 수정

--- src/pages/MyRecord/ShowMyChallenge.jsx
+++ src/pages/MyRecord/ShowMyChallenge.jsx
@@ -14,34 +14,36 @@ const ShowMyChallenge = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
-    <>
+    <div className={styles.PageContainer}>
       <Header />
-      <div className={styles.wrapper}>
-        <div className={styles.myConsTitleWrapper}>
-          <div className={styles.myConsTitle}>
-            <span>나의 소비 기록은 ?</span>
-            <img
-              src={question}
-              alt="카테고리 설명을 볼 수 있는 아이콘"
-              onClick={() => setModalOpen(true)}
-            />
-          </div>
+      <div className={styles.myConsTitleWrapper}>
+        <div className={styles.myConsTitle}>
+          <span>나의 소비 기록은 ?</span>
+          <img
+            src={question}
+            alt="카테고리 설명을 볼 수 있는 아이콘"
+            onClick={() => setModalOpen(true)}
+          />
         </div>
+      </div>
+      <div style={{ marginRight: "20px" }}>
         <Dropdown
           sortArr={sortChallenge}
           setSortArr={setSortChallenge}
           sortType={sortType}
         />
+      </div>
+      <div className={styles.wrapper}>
         <div className={styles.challengeRecordWrapper}>
           {sortChallenge.map((item) => (
-            <MyChallengeRecord id={item.id} item={item} isConsData={true} />
+            <MyChallengeRecord key={item.id} item={item} isConsData={true} />
           ))}
         </div>
         {modalOpen && (
           <DescModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
         )}
       </div>
-    </>
+    </div>
   );
 };
 
@@ -80,6 +82,36 @@ const dummyData = [
   },
   {
     id: 4,
+    title: "4회차",
+    cons: 60000,
+    target: 100000,
+    startDate: "2025-11-10",
+    endDate: "2025-11-18",
+    coin: 400,
+    isSuccessed: true,
+  },
+  {
+    id: 5,
+    title: "2회차",
+    cons: 100000,
+    target: 70000,
+    startDate: "2025-12-10",
+    endDate: "2025-12-18",
+    coin: 300,
+    isSuccessed: false,
+  },
+  {
+    id: 6,
+    title: "3회차",
+    cons: 50000,
+    target: 45000,
+    startDate: "2024-11-10",
+    endDate: "2024-11-18",
+    coin: 500,
+    isSuccessed: false,
+  },
+  {
+    id: 7,
     title: "4회차",
     cons: 60000,
     target: 100000,
