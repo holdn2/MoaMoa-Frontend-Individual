3c544ce30832eb66a2a35018be3d72080ac66c25
JoGyuBin <128724038+rbqks529@users.noreply.github.com>
2025-02-21 00:28:45 +0900

    Revert "Revert "[Feat] queryString으로 token 받아와서 localStorage에 저장하기""

--- src/apis/friend.js
+++ src/apis/friend.js
@@ -5,14 +5,8 @@ import axiosInstance from "./axiosInstance";
 // 친구 목록 전체 보기 api
 export const getAllFriendsInfo = async (setAllFriendsData) => {
   try {
-    const response = await axios.get(
-      "https://moamoa.store/friendships/my-friend",
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoiUk9MRV9BRE1JTiIsImlhdCI6MTczODQ4NjQ0OSwiZXhwIjoxNzQxMDc4NDQ5fQ.tccAfZ4LfshBl5S8n1lgj5pfrU9VybbNyulS7aZGXyc",
-        },
-      }
+    const response = await axiosInstance.get(
+      "/friendships/my-friend",
     );
     console.log(response.data.result);
     setAllFriendsData(response.data.result);
@@ -24,14 +18,8 @@ export const getAllFriendsInfo = async (setAllFriendsData) => {
 // 친구 추가를 위한 검색 api
 export const searchNewFriends = async (friendNickname, setSearchedFriends) => {
   try {
-    const response = await axios.get(
-      `https://moamoa.store/friendships/search?nickname=${friendNickname}`,
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoiUk9MRV9BRE1JTiIsImlhdCI6MTczODQ4NjQ0OSwiZXhwIjoxNzQxMDc4NDQ5fQ.tccAfZ4LfshBl5S8n1lgj5pfrU9VybbNyulS7aZGXyc",
-        },
-      }
+    const response = await axiosInstance.get(
+      `/friendships/search?nickname=${friendNickname}`,
     );
     setSearchedFriends(response.data.result);
   } catch (error) {
@@ -42,15 +30,9 @@ export const searchNewFriends = async (friendNickname, setSearchedFriends) => {
 // 친구 추가 요청 api
 export const postFriendRequest = async (userId) => {
   try {
-    const response = await axios.post(
-      `https://moamoa.store/friendships/request/${userId}`,
+    const response = await axiosInstance.post(
+      `/friendships/request/${userId}`,
       {},
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoiUk9MRV9BRE1JTiIsImlhdCI6MTczODQ4NjQ0OSwiZXhwIjoxNzQxMDc4NDQ5fQ.tccAfZ4LfshBl5S8n1lgj5pfrU9VybbNyulS7aZGXyc",
-        },
-      }
     );
   } catch (error) {
     console.error("Error request add friend :", error);
@@ -60,17 +42,11 @@ export const postFriendRequest = async (userId) => {
 // 친구 요청 수락 api
 export const acceptFriendRequest = async (notificationId, isAccept) => {
   try {
-    const response = await axios.post(
-      `https://moamoa.store/friendships/request/${notificationId}/action`,
+    const response = await axiosInstance.post(
+      `/friendships/request/${notificationId}/action`,
       {
         request: isAccept,
       },
-      {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoiUk9MRV9BRE1JTiIsImlhdCI6MTczODQ4NjQ0OSwiZXhwIjoxNzQxMDc4NDQ5fQ.tccAfZ4LfshBl5S8n1lgj5pfrU9VybbNyulS7aZGXyc",
-        },
-      }
     );
   } catch (error) {
     console.error("Error request add friend :", error);
